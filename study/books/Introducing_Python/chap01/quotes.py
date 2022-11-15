83030c905b43fb56628419a7731976c889b261db
prifri <kkr1211@naver.com>
2022-11-16 01:27:21 +0900

    p43

---
+++ study/books/Introducing_Python/chap01/quotes.py
@@ -0,0 +1,11 @@
+''''
+주석인가
+'''
+
+quotes = {
+    "Moe" : "A wise guy, huh?",
+    "Larry" : "Ow!",
+    "Curly": "Nyuk nyuk!"
+}
+stooge = "Curly"
+print(stooge, "says:", quotes[stooge])
---
+++ study/books/Introducing_Python/chap01/spells.py
@@ -0,0 +1,8 @@
+spell = [
+    "a",
+    "b",
+    "c",
+    "d",
+]
+
+print(spell[3])
