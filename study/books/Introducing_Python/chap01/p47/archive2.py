21d4f4e0fd01ad1f7cd9a1065580d4969a7c6f3d
prifri <kkr1211@naver.com>
2022-11-20 00:06:41 +0900

    chap01

--- study/books/Introducing_Python/chap01/p47/archive2.py
+++ study/books/Introducing_Python/chap01/p47/archive2.py
@@ -1,15 +1,15 @@
+"""
+pip.exe install requests
+"""
 import webbrowser
-import json
-from urllib.request import urlopen
+import requests
 
 print("Let's find an old website.")
 site = input("Type a website URL: ")
 era = input("Type a year, month, and day, like 20150613: ")
 url = "https://archive.org/wayback/available?url=%s&timestamp=%s" % (site, era)
-response = urlopen(url)
-contents = response.read()
-text = contents.decode("utf-8")
-data = json.loads(text)
+response = requests.get(url)
+data = response.json()
 try:
     old_site = data["archived_snapshots"]["closest"]["url"]
     print("Found this copy: ", old_site)
