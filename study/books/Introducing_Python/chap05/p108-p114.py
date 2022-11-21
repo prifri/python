cfa754e081ee0dfd46977e5918e6e6ec489a37d4
prifri <kkr1211@naver.com>
2022-11-22 00:52:33 +0900

    p115

---
+++ study/books/Introducing_Python/chap05/p108-p114.py
@@ -0,0 +1,49 @@
+print('Snap')
+print("Crakle")
+print("Abc'abc'")
+print('Abc"abc"')
+print('''abc''')
+print("""abc""")
+poem = '''there was,
+Whho;
+    When ,
+ She,
+This'''
+print(poem)
+print('Give', "us", '''Some''', """space""")
+print('')
+print("")
+print('''''')
+print("""""")
+
+print(str(98.6))
+print(str(1.0e4))
+print(str(True))
+print(str(None))
+print('A\nB\nC\tD')
+
+print("\"")
+print('\'')
+print('\\')
+
+#원시 문자열. \를 무시한다.
+print(r'abc\ncde')
+
+print("abc" + "cde")
+print("123" "456")
+vowels = ( 'a'
+'b'
+'c' '''d'''
+)
+print(vowels)
+
+a = 'Duck.'
+b = a
+c = 'Grey Duck!'
+print(a + b + c)
+print(a, b, c)
+
+start = 'Na ' * 4 + '\n'
+middle = 'Hey ' * 3 + '\n'
+end = 'Goobdye'
+print(start + start + middle + end)
---
+++ study/books/Introducing_Python/chap05/p114-p115.py
@@ -0,0 +1,11 @@
+letters = 'abc'
+print(letters[0])
+print(letters[1])
+print(letters[-1])
+print(letters[-3])
+#erro print(letters[-4])
+name = 'HHnny'
+name2 = name.replace('H','P')
+print(name)
+print(name2)
+print('X' + name[1:])
